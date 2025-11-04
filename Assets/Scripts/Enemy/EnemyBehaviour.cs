7af8a2895d494307d979a747b5c6eb3627bd2f78
Lukas Bijelic <Bijelic.Lukas.2003@gmail.com>
2025-11-04 01:15:13 +0100

    Collision added
    The Enemy can now collide with the Player and will self-delete itself.

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -3,41 +3,12 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    //[SerializeField] Vector3 PlayerPosition;
+    [SerializeField] GameObject Player;
     [SerializeField] float Speed;
-    [SerializeField] float minX,maxX,minZ,maxZ;
-    [SerializeField] float Cooldown;
-    float lastTime;
-    float cooldownTimer;
-    Vector3 PlayerPosition;
-
-    private void Start()
-    {
-        lastTime = Time.time;
-        cooldownTimer = 0;
-    }
 
     private void Update()
     {
-        //if (lastTime + Cooldown < Time.time)
-        //{
-        //    float randomX = Random.Range(minX, maxX);
-        //    float randomZ = Random.Range(minZ, maxZ);
-        //    PlayerPosition = new Vector3(randomX, 0, randomZ);
-        //    Debug.Log(PlayerPosition);
-        //    lastTime = Time.time;
-        //}
-
-        if (cooldownTimer > Cooldown)
-        {
-            float randomX = Random.Range(minX, maxX);
-            float randomZ = Random.Range(minZ, maxZ);
-            PlayerPosition = new Vector3(randomX, 0, randomZ);
-            Debug.Log(PlayerPosition);
-            cooldownTimer = 0;
-        }
-
-        Vector3 direction = Vector3.Normalize(PlayerPosition - transform.position);
+        Vector3 direction = Vector3.Normalize(Player.transform.position - transform.position);
         direction.y = 0;
         //Debug.Log($"Direction: {direction}");
         if(direction.x != 0 && direction.z != 0)
@@ -45,6 +16,11 @@ public class EnemyBehaviour : MonoBehaviour
             transform.forward = direction;
             transform.position += direction * Time.deltaTime * Speed;
         }
-        cooldownTimer += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag != "player") return;
+        Destroy(gameObject);
     }
 }
